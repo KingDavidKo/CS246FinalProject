853236d3701d91c5dba18829b29003eef4908100
sljilla <sljilla@uwaterloo.ca>
2024-11-29 14:58:54 -0500

    added command line interpretor

--- main.cc
+++ main.cc
@@ -1,12 +1,32 @@
 #include "gameController.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, char const *argv[]) {
+    int level = 0;
+    int seed = -1;
+    bool textOnly = false;
+    std::string scriptFile1 = "sequence1.txt";
+    std::string scriptFile2 = "sequence2.txt";
 
-    // so we need to get the level from command line arguments, if there are any!
-    gameController game = gameController(0, true);
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        if (arg == "-text") {
+            textOnly = true;
+        } else if (arg == "-seed" && i + 1 < argc) {
+            seed = std::stoi(argv[++i]);
+        } else if (arg == "-scriptfile1" && i + 1 < argc) {
+            scriptFile1 = argv[++i];
+        } else if (arg == "-scriptfile2" && i + 1 < argc) {
+            scriptFile2 = argv[++i];
+        } else if (arg == "-startlevel" && i + 1 < argc) {
+            level = std::stoi(argv[++i]);
+        } else {
+            break;
+        }
+    }
+    gameController game(level, textOnly, seed, scriptFile1, scriptFile2);
     game.run();
     return 0;
-
 }
-
-
